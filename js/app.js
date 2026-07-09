a762cc30ca7c86bfca506b7896309745377650d3
Tony Riggs <triggs@nscshirts.com>
2026-07-08 23:43:43 -0700

    Switch to classic GitHub token

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@
     repoOwner: 'triggs2025',
     repoName: 'azvlc',
     branch: 'master',
-    ghToken: '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'
+    ghToken: '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'
   };
 
   // ── State ──
